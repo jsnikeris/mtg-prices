7034b2b6ff1ac56894f095e01ef85769a2e52a09
Joe Snikeris <joe@snikeris.com>
2011-11-27 17:57:12 -0500

    add fn for fetching set as seq of maps

---
+++ src/mtg_prices/core.clj
@@ -0,0 +1,32 @@
+(ns mtg-prices.core
+  (:import [java.net URL URLEncoder])
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.string :as str]))
+
+(def guide-url
+  "http://magic.tcgplayer.com/db/price_guide.asp?setname=")
+
+(def sets
+  ["Scars of Mirrodin"
+   "Mirrodin Besieged"
+   "New Phyrexia"
+   "Magic 2012 (M12)"
+   "Innistrad"])
+
+(defn extract-price [price-node]
+  (->> price-node html/text (re-find #"\d+\.\d+") Double.))
+
+(defn fetch-set [name]
+  (-> (str guide-url (URLEncoder/encode name "utf-8")) URL. html/html-resource
+      (html/select [[:table (html/nth-of-type 3)] :tr])
+      (html/let-select [[rarity]    [[:td (html/nth-of-type 5)]]
+                        [avg-price] [[:td (html/nth-of-type 7)]]
+                        [low-price] [[:td (html/nth-of-type 8)]]]
+        {:rarity
+         (case (->> rarity html/text (re-find #"[CURM]"))
+           "C" :common
+           "U" :uncommon
+           "R" :rare
+           "M" :mythic)
+         :avg-price (extract-price avg-price)
+         :low-price (extract-price low-price)})))
